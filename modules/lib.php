67255d065d70d3db7818d869dfc5f3e2eb13c1d3
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-01-14 16:16:44 -0300

    Update lib.php - Changed include process

--- modules/lib.php
+++ modules/lib.php
@@ -1,19 +1,18 @@
 <?php
-
 //define('GOT_CONFIG', true);
 //arquivo comum
 //carrega-lo em todos os arquivos que necessitarem as bibliotecas
 //carregar neste arquivo os plugins necessários
 //atentar que alguns dependem de outros, então existe ordem de importação
 include('config.php');
-include('modules/conn.php');
-include('modules/general.php');
-include('modules/player.php');
-include('modules/game.php');
-include('modules/pokemon.php');
-include('modules/logs.php');
-include('modules/security.php');
-include('modules/language.php');
+include('conn.php');
+include('general.php');
+include('player.php');
+include('game.php');
+include('pokemon.php');
+include('logs.php');
+include('security.php');
+include('language.php');
 
 
 ?>
