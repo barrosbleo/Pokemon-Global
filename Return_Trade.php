947c409d207bd69cb370440b8cc17d2874cb1412
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-02-04 10:53:25 -0300

    Add files via upload

--- Return_Trade.php
+++ Return_Trade.php
@@ -1,25 +1,25 @@
  <?php
+ //guess this is for cancel all user trades
+ //move this to staff section
 die();
 include('modules/lib.php');
-    //include 'config.php';
-     
-    $query = "select * from `offer_pokemon`";
-     
-	$result = $conn->query($query);
-    while ($row = $result->fetch_assoc()) {
-        $row['gender'] = rand(0, 2);
-        $query2 = $conn->query("
-           INSERT INTO `user_pokemon` (
-               `uid`, `name`, `level`, `exp`, `move1`, `move2`, `move3`, `move4`, `gender`
-           ) VALUES (
-               '{$row['uid']}', '{$row['name']}', '{$row['level']}', '{$row['exp']}', '{$row['move1']}', '{$row['move2']}', '{$row['move3']}', '{$row['move4']}', '{$row['gender']}'
-           )
-       ");
-     
-        if ($query2) {
-            $conn->query("delete from `offer_pokemon` where `id`='{$row['id']}'");
-        }
-    }
-     
+
+$query = "select * from `offer_pokemon`";
+
+$result = $conn->query($query);
+while ($row = $result->fetch_assoc()) {
+	$row['gender'] = rand(0, 2);
+	$query2 = $conn->query("
+		INSERT INTO `user_pokemon` (
+		`uid`, `name`, `level`, `exp`, `move1`, `move2`, `move3`, `move4`, `gender`
+		) VALUES (
+		'{$row['uid']}', '{$row['name']}', '{$row['level']}', '{$row['exp']}', '{$row['move1']}', '{$row['move2']}', '{$row['move3']}', '{$row['move4']}', '{$row['gender']}'
+		)
+	");
+
+	if ($query2) {
+		$conn->query("delete from `offer_pokemon` where `id`='{$row['id']}'");
+	}
+}   
 ?>
 
