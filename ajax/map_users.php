afa4a2b09ae4ac0f1fd15157eb8869af14ad6199
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-02-17 15:09:32 -0300

    Add files via upload

--- ajax/map_users.php
+++ ajax/map_users.php
@@ -2,7 +2,7 @@
 include('../modules/lib.php');
 
 $uid = (int) $_SESSION['userid'];
-$map = (int) $_GET['map'];
+$map = (int) base64_decode($_GET['map']);
 
 $time = time();
 $minsAgo = $time - (60*1);//60 second * 1 minute
@@ -16,7 +16,7 @@ while ($user = $result->fetch_assoc()) {
 		continue;
 	}
 	$usersArray[$i]['username'] = cleanHtml($user['username']);
-	$usersArray[$i]['sprite']   = (int) $user['map_sprite'];
+	$usersArray[$i]['sprite']   = $user['map_sprite'];
 	$usersArray[$i]['id'] = (int) $user['id'];
 	$usersArray[$i]['x']  = (int) $user['map_x'];
 	$usersArray[$i]['y']  = (int) $user['map_y'];
