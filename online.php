38bf1caba8a64c874be766248ea65f50180dbf78
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-02-17 14:05:41 -0300

    Add files via upload

--- online.php
+++ online.php
@@ -10,7 +10,7 @@ printHeader($lang['online_title']);
 
 $uid = (int) $_SESSION['userid'];
 $time = time();
-$otime = $time - (60*60);//time to calc (1 hour)
+$otime = $time - (60*5);//time to calc (1 hour)
 
 /// just an test -- >
 if (isset($_GET['get'])) {
