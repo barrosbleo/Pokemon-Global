2ab4a92dc5756f07333ac26d5c2efaa0fa40e91c
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-02-23 15:29:29 -0300

    Add files via upload

--- legalinfo.php
+++ legalinfo.php
@@ -18,7 +18,7 @@ include '_header.php';
 							</a>
 							<br>
 							<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"><?php echo $lang['footer_cr_title'];?></span> 
-							by <a xmlns:cc="http://creativecommons.org/ns#" href="http://pokeglobal.com" property="cc:attributionName" rel="cc:attributionURL">Luke</a> 	
+							by <a xmlns:cc="http://creativecommons.org/ns#" href="https://pkmglobal.online" property="cc:attributionName" rel="cc:attributionURL">Luke</a> 	
 							is licensed under a 
 							<a rel="license" href="http://creativecommons.org/licenses/by/2.0/uk/deed.en_US">Creative Commons Attribution 2.0 UK: England &amp; Wales License</a>.
 							<br>
@@ -26,9 +26,9 @@ include '_header.php';
 							<a xmlns:dct="http://purl.org/dc/terms/" href="http://pkmnhelios" rel="dct:source">http://pkmnhelios.net</a>.
 							<br>
 							Permissions beyond the scope of this license may be available at 
-							<a xmlns:cc="http://creativecommons.org/ns#" href="http://pokeglobal.com/legalinfo.php" rel="cc:morePermissions">http://pokeglobal.com/legalinfo.php</a>.
+							<a xmlns:cc="http://creativecommons.org/ns#" href="https://pkmglobal.online/legalinfo.php" rel="cc:morePermissions">https://pkmglobal.online/legalinfo.php</a>.
 						</p>
-						<p>If you have any inquires please contact the owner on contato@pokeglobal.com</p>
+						<p>If you have any inquires please contact the owner on support@pkmglobal.online</p>
 					</div>
 					<?php include '_footer.php'; ?>
 				</td>
