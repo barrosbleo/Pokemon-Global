068414d90c056be964373ed0af7b2501ac87ae15
barrosbleo <66649140+barrosbleo@users.noreply.github.com>
2021-05-24 15:47:16 -0300

    Add files via upload

--- modules/tutorial.php
+++ modules/tutorial.php
@@ -1,7 +1,17 @@
 <?php
 include('lib.php');
 
-if(isset($_GET['id']) && isset($_GET['func']) && $_GET['func'] == "next"){
+if(isset($_GET['id']) && isset($_GET['func']) && $_GET['func'] == "done"){
+	$uid = cleanSql($_GET['id'], $conn);
+	$query = "UPDATE users SET tuto = '10' WHERE id = '{$uid}'";
+	$conn->query($query);
+	$query = "SELECT tuto FROM users WHERE id = '{$uid}'";
+	$result = $conn->query($query);
+	$tutoNum = $result->fetch_assoc();
+	$_SESSION['tuto'] = $tutoNum['tuto'];
+	echo $tutoNum['tuto'];
+	die();
+}else if(isset($_GET['id']) && isset($_GET['func']) && $_GET['func'] == "next"){
 	$uid = cleanSql($_GET['id'], $conn);
 	$query = "UPDATE users SET tuto = tuto + 1 WHERE id = '{$uid}'";
 	$conn->query($query);
